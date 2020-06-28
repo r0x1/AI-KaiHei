501ea8cb3a11b747295cc47dc4b7318f5984e02b
r0x1 <60580333+r0x1@users.noreply.github.com>
2020-06-28 14:47:16 +0800

    init yolo5

---
+++ brawl_stars/object_detect/yolov5/detect_brawl_stars.py
@@ -0,0 +1,4 @@
+
+# cd C:\Users\dyh\workspace\AI-KaiHei\brawl_stars
+# python object_detect/yolo5/detect.py --weights object_detect/yolo5/weights/air_force_r_yolov5s_results.pt --img 1088 --conf 0.4 --source object_detect/yolo5/content/test/images
+
---
+++ main.py
@@ -0,0 +1,5 @@
+
+# print('hello!')
+import os
+
+print(os.sys.path)
